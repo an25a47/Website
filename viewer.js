f3dcc941a6dc9d96582cd9945c3c5741efc3ebe2
an25a47 <134784571+an25a47@users.noreply.github.com>
2023-06-22 22:35:06 +0100

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,30 +1,19 @@
-const remoteVideo = document.getElementById('remoteVideo');
+// Similar changes as in main.js
 
-function startPeerConnection() {
-    const configuration = {
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-    };
-    const peerConnection = new RTCPeerConnection(configuration);
-
-    peerConnection.ontrack = event => {
-        remoteVideo.srcObject = event.streams[0];
-    };
-
-    // When you receive the offer from the other peer over your server
-    peerConnection.setRemoteDescription(offer)
-        .then(() => {
-            return peerConnection.createAnswer();
-        })
-        .then(answer => {
-            return peerConnection.setLocalDescription(answer);
-        })
-        .then(() => {
-            // Send peerConnection.localDescription back to the other peer over your server
-        });
-
-    peerConnection.onicecandidate = event => {
-        if (event.candidate) {
-            // Send the candidate to the other peer over your server
-        }
-    };
+function sendMessage(message) {
+    if (connection) {
+        connection.sendUTF(JSON.stringify(message));
+    }
 }
+
+const connection = new WebSocket('ws://localhost:3000');
+
+connection.onmessage = message => {
+    const data = JSON.parse(message.data);
+
+    if (data.offer) {
+        startPeerConnection(data.offer);
+    } else if (data.candidate) {
+        peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+    }
+};
