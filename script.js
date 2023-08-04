e6a5652920cf56779dac869b901b21e67529ab50
an25a47 <134784571+an25a47@users.noreply.github.com>
2023-08-04 11:10:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function createConfetti() {
-    const confettiContainer = document.querySelector(".confetti");
+    const confettiContainer = document.querySelector(".confetti-container");
 
     for (let i = 0; i < 100; i++) {
         const confetti = document.createElement("div");
         confetti.classList.add("confetti");
         confetti.style.left = `${Math.random() * 100}vw`;
-        confetti.style.animationDelay = `${Math.random() * 4}s`;
+        confetti.style.animationDuration = `${Math.random() * 3 + 2}s`;
+        confetti.style.animationDelay = `${Math.random() * 2}s`;
         confettiContainer.appendChild(confetti);
     }
 }
